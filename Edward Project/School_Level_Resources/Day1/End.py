6f142faea049c178a8a2e0f244f45272f96865e1
Keem321 <47438044+Keem321@users.noreply.github.com>
2019-02-07 22:19:17 -0500

    Testing commit

--- Edward Project/School_Level_Resources/Day1/End.py
+++ Edward Project/School_Level_Resources/Day1/End.py
@@ -4,3 +4,4 @@ import Control as c
 def run():
     print("TEST: DAY1_End is ready for writing!!")
 
+# There should be a message informing the player how well they did over the day / how good they are doing overall
